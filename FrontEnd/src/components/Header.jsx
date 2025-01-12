ca12dce3a802557cd9ffcdeb67ec6b72c53dc86f
Rauf005 <rauffa-af206@code.edu.az>
2025-01-12 22:17:15 +0400

    last

--- FrontEnd/src/components/Header.jsx
+++ FrontEnd/src/components/Header.jsx
@@ -1,11 +1,69 @@
 import React from 'react'
 import {NavLink} from "react-router-dom"
 import style from "./style.module.css"
+import respons from "../responsive.module.css"
+import { FaBars } from "react-icons/fa";
 
 function Header() {
   return (
     <div className={style.navbar} >
-        <ul>
+      <div className={respons.dd}>
+      <div style={{fontSize:"25px"}} className={respons.bar} > <FaBars/></div>
+      <ul className={respons.rul}>
+        <NavLink
+        
+            className={respons.td}
+
+            to="/"
+            style={({ isActive }) => {
+              return isActive ? { color: "#F6214B" } : {};
+            }}
+          >
+            <li>Home</li>
+          </NavLink>
+          <NavLink
+          style={({ isActive }) => {
+            return isActive ? { color: "#F6214B" } : {};
+          }}
+          to="favorites"
+            className={respons.td}
+          >
+            <li>Wishlist</li>
+          </NavLink>
+          <NavLink
+            className={respons.td}
+          >
+            <li>Immigration</li>
+          </NavLink>
+          <NavLink
+            className={respons.td}
+          >
+            <li>Course</li>
+          </NavLink>
+          <NavLink
+            className={respons.td}
+          >
+            <li>Country</li>
+          </NavLink>
+          <NavLink
+            className={respons.td}
+          >
+            <li>Blog</li>
+          </NavLink>
+          <NavLink
+            className={respons.td}
+          >
+            <li>Contact</li>
+          </NavLink>
+          <NavLink
+            className={respons.td}
+          >
+            <li>Element</li>
+          </NavLink>
+
+        </ul>
+      </div>
+        <ul className={respons.ul}>
         <NavLink
         
             className={style.td}
